78821b60d07af95282e6fa7762e948dd2b7dbf0d
Daniel Mayer <gnummig@gmail.com>
2017-05-15 09:39:45 +0200

    some -Y flags and a forgotten cd ..

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-#Start with fresh  5.14 jessie server image. Bring it up to 5.16
-apt-get update
-apt-get upgrade
+#Start with fresh  5.25 jessie server image.
+apt-get update -Y
+apt-get upgrade -Y
 apt-get install python-dev python-pip
 pip install spidev
 python setup.py install
@@ -9,7 +9,7 @@ pip install --upgrade pip
 sudo pip install setuptools
 sudo pip install pip
 sudo apt install python-dev
-cd ..
+#cd ..
 git clone https://github.com/duxingkei33/orangepi_PC_gpio_pyH3
 cd orangepi_PC_gpio_pyH3
 python setup.py install
