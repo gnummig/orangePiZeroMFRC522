8d4ae22210b01fed615108e09fe7b63d7ae2fd59
Daniel Mayer <gnummig@gmail.com>
2017-06-13 16:31:27 +0200

    no sudo for pip in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,14 +3,24 @@
 sudo apt-get --yes update
 sudo apt-get --yes upgrade 
 sudo apt-get --yes install python-dev python-pip
-sudo pip install spidev
+pip install spidev --user
 #python setup.py install
-pip install --upgrade pip
-sudo pip install setuptools
-sudo pip install pip
-sudo apt install python-dev
+pip install --upgrade pip --user
+pip install setuptools --user
+pip install pip --user
+#apt install python-dev --user
+# change owndership of the python library folder to be able to run librarys as user door
+#sudo chown -R door /usr/local/lib/
+# get gpio library
 git clone https://github.com/duxingkei33/orangepi_PC_gpio_pyH3
-python orangepi_PC_gpio_pyH3/setup.py install
+cd orangepi_PC_gpio_pyH3/
+python setup.py install
+cd ..
+#get SPI library
 git clone https://github.com/lthiery/SPI-Py.git
-python SPI-Py/setup.py install
+cd SPI-Py/
+python setup.py install
+cd ..
+#get MFRC533 library
 git clone https://github.com/gnummig/MFRC522-python
+sudo sed -i "/exit 0/c\ exec sudo python \/home\/door\/orangePiZeroMFRC522\/door.py & \n exit 0" /etc/rc.local 
