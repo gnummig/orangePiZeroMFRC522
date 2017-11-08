c44845bde71e6d7e5ae35875084e7a2620d9a271
Daniel Mayer <gnummig@gmail.com>
2017-11-08 12:40:13 +0100

    no idea what happened

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 #Start with fresh Armbian_5.25_Orangepizero_Debian_jessie_default_3.4.113.img
-printf "for wifi usage please enter the BSSID and the password of your wifi\n be aware that to this point the password is saved as plaintext on the operating system \n SSID:"
-read wifissid
-printf "password:"
-read wifipasswd
+#printf "for wifi usage please enter the BSSID and the password of your wifi\n be aware that to this point the password is saved as plaintext on the operating system \n SSID:"
+#read wifissid
+#printf "password:"
+#read wifipasswd
 apt-key update
 apt-get --yes update
 apt-get --yes upgrade 
@@ -42,10 +42,10 @@ cd ..
 #cd /etc/init.d/
 #update-rc.d initdoor.sh defaults
 # configure wifi( not sure whether it works:
-cp /etc/network/interfaces /etc/network/interfacesoriginal
-rm /etc/network/interfaces
-printf "iface lo inet loopback\n\nauto eth0\nallow-hotplug eth0\niface eth0 inet dhcp\n\nallow-hotplug wlan0\niface wlan0 inet manual\nwpa-roam /etc/wpa_supplicant/wpa_supplicant.conf\niface default inet dhcp" > /etc/network/interfaces
-printf "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\n\nnetwork={\nssid=\""$wifissid\""\npsk=\""$wifipasswd\""\n}" > /etc/wpa_supplicant/wpa_supplicant.conf
+#cp /etc/network/interfaces /etc/network/interfacesoriginal
+#rm /etc/network/interfaces
+#printf "iface lo inet loopback\n\nauto eth0\nallow-hotplug eth0\niface eth0 inet dhcp\n\nallow-hotplug wlan0\niface wlan0 inet manual\nwpa-roam /etc/wpa_supplicant/wpa_supplicant.conf\niface default inet dhcp" > /etc/network/interfaces
+#printf "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\n\nnetwork={\nssid=\""$wifissid\""\npsk=\""$wifipasswd\""\n}" > /etc/wpa_supplicant/wpa_supplicant.conf
 #make the orPi slow so that it doesnt consume to much energy:
 echo 0 >/sys/devices/system/cpu/cpu3/online
 echo 0 >/sys/devices/system/cpu/cpu2/online
